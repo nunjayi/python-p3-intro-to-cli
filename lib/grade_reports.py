bb577c6324369193ac7ebff2383364ff40493a75
nunjayi <epenyash@gmail.com>
2024-06-12 03:25:10 +0300

    cli program

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
+def create_grade_report(student_grades):
+    with open('reports/grade_report.txt', 'w') as gr:
+        gr.write(student_grades)
+
+
+
 if __name__ == '__main__':
-    pass
+    student_grades = input("Student name, grade: ")
+    create_grade_report(student_grades)
